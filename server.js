d486faf8b686d1e320fbd0da5d8ac8052037625b
Sushil <sushilrawat1720@gmail.com>
2024-02-04 13:54:24 +0530

    index

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ const connectDatabase = require("./config/database");
 const PORT = 3001;
 
 connectDatabase();
-
+app.use("/", (req, res) => {
+  res.json({
+    message: "Something ",
+  });
+});
 app.listen(PORT, () => {
   console.log(`Server is running on PORT http://localhost:${PORT}`);
 });
