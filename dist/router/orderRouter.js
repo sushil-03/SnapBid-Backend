b123d0c8c67427352f8442fd39a83328219eea06
Sushil <sushilrawat1720@gmail.com>
2024-05-11 23:16:31 +0530

    Renderer upgrade node version

--- dist/router/orderRouter.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const orderController_1 = require("../controller/orderController");
-const auth_1 = require("../middleware/auth");
-const router = (0, express_1.Router)();
-router.route("/order").get(auth_1.isAuthenticated, orderController_1.fetchOrderByUserType);
-router.route("/order").post(auth_1.isAuthenticated, orderController_1.updateOrder);
-exports.default = router;
--- dist/router/serviceRouter.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const router = (0, express_1.Router)();
-const serviceController_1 = require("../controller/serviceController");
-router.route("/service/otp").post(serviceController_1.generateOTP);
-exports.default = router;
